51fb628ce0d1d8e6cd60eab38f7e6dc95cedbbb8
YeaMerci <113313229+YeaMerci@users.noreply.github.com>
2023-09-04 14:15:50 +0300

    feat: added pages Health & Research

---
+++ pages/0_💊_Health.py
@@ -0,0 +1,21 @@
+"""Page about health"""
+
+import streamlit as st
+from pathlib import Path
+
+
+# --- PATH SETTINGS ---
+APP_DIR: Path = Path(__file__).parent.parent if "__file__" in locals() else Path.cwd().parent
+ASSETS_DIR: Path = APP_DIR / "assets"
+GIF_DIR: Path = ASSETS_DIR / "gif"
+IMAGES_DIR: Path = ASSETS_DIR / "images"
+
+# --- GENERAL SETTINGS ---
+PAGE_TITLE: str = "Health"
+PAGE_ICON: str = "💊"
+
+
+st.set_page_config(
+    page_title=PAGE_TITLE,
+    page_icon=PAGE_ICON
+)
