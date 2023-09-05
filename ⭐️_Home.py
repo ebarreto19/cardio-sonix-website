350f3277045f669c4e994789219511bf5d677520
YeaMerci <113313229+YeaMerci@users.noreply.github.com>
2023-09-05 19:06:29 +0300

    style: added text into div blocks

--- ⭐️_Home.py
+++ ⭐️_Home.py
@@ -35,13 +35,16 @@ with col1:
     st.image(f"{IMAGES_DIR}/heart-attack.png")
     st.write(
         """
-        According to the World Health Organisation, 
-        cardiovascular diseases (CVDs) are the number one cause of death globally: 
-        more people die annually from CVDs than from any other cause. 
-        An estimated 17.1 million people died from CVDs in 2004, 
-        representing 29% of all global deaths. 
-        Of these deaths, an estimated 7.2 million were due to coronary heart disease.
-        """
+        <div class="alert alert-block alert-info" style="font-size:20px; background-color: #0b0e22; font-family:verdana; color: #ffffff; border-radius: 10px; border: 0px #533078 solid">
+            According to the World Health Organisation, 
+            cardiovascular diseases (CVDs) are the number one cause of death globally: 
+            more people die annually from CVDs than from any other cause. 
+            An estimated 17.1 million people died from CVDs in 2004, 
+            representing 29% of all global deaths. 
+            Of these deaths, an estimated 7.2 million were due to coronary heart disease.
+        </div>
+        """,
+        unsafe_allow_html=True
     )
 
 
@@ -50,13 +53,16 @@ with col2:
     st.image(f"{GIF_DIR}/slime.gif")
     st.write(
         """
-        Any method which can help to detect signs of heart disease 
-        could therefore have a significant impact on world health. 
-        This challenge is to produce methods to do exactly that. 
-        Specifically, we are interested in creating the first level of screening 
-        of cardiac pathologies both in a Hospital environment by a doctor 
-        (using a digital stethoscope) and at home by the patient (using a mobile device).
-        """
+        <div class="alert alert-block alert-info" style="font-size:20px; background-color: #0b0e22; font-family:verdana; color: #ffffff; border-radius: 10px; border: 0px #533078 solid">
+            Any method which can help to detect signs of heart disease 
+            could therefore have a significant impact on world health. 
+            This challenge is to produce methods to do exactly that. 
+            Specifically, we are interested in creating the first level of screening 
+            of cardiac pathologies both in a Hospital environment by a doctor 
+            (using a digital stethoscope) and at home by the patient (using a mobile device).
+        </div>
+        """,
+        unsafe_allow_html=True
     )
 
 
@@ -64,16 +70,19 @@ st.header(":blue[Medicine and AI]")
 st.image(f"{IMAGES_DIR}/medicine-ai.jpeg")
 st.write(
     """
-    The problem is of particular interest to machine learning researchers 
-    as it involves classification of audio sample data, 
-    where distinguishing between classes of interest is non-trivial. 
-    Data is gathered in real-world situations 
-    and frequently contains background noise of every conceivable type. 
-    The differences between heart sounds corresponding 
-    to different heart symptoms can also be extremely subtle 
-    and challenging to separate. Success in classifying this 
-    form of data requires extremely robust classifiers. 
-    Despite its medical significance, 
-    to date this is a relatively unexplored application for machine learning.
-    """
+    <div class="alert alert-block alert-info" style="font-size:20px; background-color: #0b0e22; font-family:verdana; color: #ffffff; border-radius: 10px; border: 0px #533078 solid">
+        The problem is of particular interest to machine learning researchers 
+        as it involves classification of audio sample data, 
+        where distinguishing between classes of interest is non-trivial. 
+        Data is gathered in real-world situations 
+        and frequently contains background noise of every conceivable type. 
+        The differences between heart sounds corresponding 
+        to different heart symptoms can also be extremely subtle 
+        and challenging to separate. Success in classifying this 
+        form of data requires extremely robust classifiers. 
+        Despite its medical significance, 
+        to date this is a relatively unexplored application for machine learning.
+    </div>
+    """,
+    unsafe_allow_html=True
 )
