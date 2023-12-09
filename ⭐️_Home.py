3d7d583365e86c55f6b0f07290f17f348320aaa6
YeaMerci <113313229+YeaMerci@users.noreply.github.com>
2023-12-09 15:36:54 +0300

    style: refreshed home page

--- ⭐️_Home.py
+++ ⭐️_Home.py
@@ -16,7 +16,7 @@ st.set_page_config(
 )
 
 
-st.title(":red[_Dr. Cardio Sonix_] :heart:")
+st.title(":red[_Dr. Cardionix_] :heart:")
 st.write(
     """
     <div class="alert alert-block alert-info" style="font-size:20px; background-color: #661321; font-family:verdana; color: #e63c3c; border-radius: 10px; border: 0px #533078 solid">
@@ -40,12 +40,10 @@ with col1:
     st.write(
         """
         <div class="alert alert-block alert-info" style="font-size:20px; background-color: #0b0e22; font-family:verdana; color: #353cbd; border-radius: 10px; border: 0px #533078 solid">
-            According to the World Health Organisation, 
-            cardiovascular diseases (CVDs) are the number one cause of death globally: 
-            more people die annually from CVDs than from any other cause. 
-            An estimated 17.1 million people died from CVDs in 2004, 
-            representing 29% of all global deaths. 
-            Of these deaths, an estimated 7.2 million were due to coronary heart disease.
+            Heart disease has remained the leading cause of death worldwide for 20 years. 
+            However, they have never claimed as many lives as they do today. 
+            Deaths from cardiovascular disease have increased by more than 2 million since 2000, reaching nearly 9 million in 2019. 
+            Heart disease today accounts for 16% of all deaths worldwide.
         </div>
         """,
         unsafe_allow_html=True
@@ -58,12 +56,10 @@ with col2:
     st.write(
         """
         <div class="alert alert-block alert-info" style="font-size:20px; background-color: #0b0e22; font-family:verdana; color: #353cbd; border-radius: 10px; border: 0px #533078 solid">
-            Any method which can help to detect signs of heart disease 
-            could therefore have a significant impact on world health. 
-            This challenge is to produce methods to do exactly that. 
-            Specifically, we are interested in creating the first level of screening 
-            of cardiac pathologies both in a Hospital environment by a doctor 
-            (using a digital stethoscope) and at home by the patient (using a mobile device).
+            Any method that can help detect signs of cardiovascular disease early can save many lives. 
+            Our goal is to make a reliable classifier accessible to every person. 
+            We see a future world in which deaths from diseases that can be cured in their early stages will disappear forever.
+            Together we can go beyond human capabilities 🌎
         </div>
         """,
         unsafe_allow_html=True
