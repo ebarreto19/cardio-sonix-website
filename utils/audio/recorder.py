dd1939c0f7f81e6acdddb31e5d8b2d678fee7f1d
YeaMerci <113313229+YeaMerci@users.noreply.github.com>
2023-12-14 00:34:16 +0300

    style: updated audiorec component

--- utils/audio/recorder.py
+++ utils/audio/recorder.py
@@ -3,11 +3,16 @@
 __all__ = ["AudioRecorder"]
 
 from typing import Union, AnyStr, Optional
-import io
 from pathlib import Path
+import os
+import io
+from io import BytesIO
+
 import librosa
+import numpy as np
 import streamlit as st
-from st_audiorec import st_audiorec
+import streamlit.components.v1 as components
+from streamlit.components.v1.components import CustomComponent
 from .converter import AudioConverter
 from utils.variables import ROOT_DIR
 
@@ -34,8 +39,34 @@ class AudioRecorder(AudioConverter):
         self.sample_rate = sample_rate
         self.mono = mono
 
-    def chunks(self):
-        pass
+    @staticmethod
+    def __init_audiorec() -> CustomComponent:
+        # get parent directory relative to current directory
+        parent_dir = os.path.dirname(os.path.abspath(__file__))
+        # custom REACT-based component for recording client audio in browser
+        build_dir = os.path.join(parent_dir, "frontend/build")
+        # specify directory and initialize st_audiorec object functionality
+        return components.declare_component("st_audiorec", path=build_dir)
+
+    def st_audiorec(self) -> bytes:
+        st_audiorec = self.__init_audiorec()
+        # Create an instance of the component: STREAMLIT AUDIO RECORDER
+        raw_audio_data = st_audiorec()  # raw_audio_data: stores all the data returned from the streamlit frontend
+        wav_bytes = None  # wav_bytes: contains the recorded audio in .WAV format after conversion
+
+        # the frontend returns raw audio data in the form of arraybuffer
+        # (this arraybuffer is derived from web-media API WAV-blob data)
+
+        if isinstance(raw_audio_data, dict):  # retrieve audio data
+            with st.spinner('retrieving audio-recording...'):
+                ind, raw_audio_data = zip(*raw_audio_data['arr'].items())
+                ind = np.array(ind, dtype=int)  # convert to np array
+                raw_audio_data = np.array(raw_audio_data)  # convert to np array
+                sorted_ints = raw_audio_data[ind]
+                stream = BytesIO(b"".join([int(v).to_bytes(1, "big") for v in sorted_ints]))
+                # wav_bytes contains audio data in byte format, ready to be processed further
+                wav_bytes = stream.read()
+        return wav_bytes
 
     def __check_duration(self, data: AnyStr | bytes) -> io.BytesIO:
         audio, sr = librosa.load(io.BytesIO(data), sr=self.sample_rate, mono=self.mono)
@@ -52,7 +83,7 @@ class AudioRecorder(AudioConverter):
             )
 
     def _record_audio(self) -> AnyStr:
-        data = st_audiorec()
+        data = self.st_audiorec()
         if data is not None:
             return self.__check_duration(data)
 
